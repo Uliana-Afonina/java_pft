3f79679e1fcc0015c1eb48e24c3ad5717399da5e
Uliana-Afonina <afonina6233@gmail.com>
2020-10-12 17:44:47 +0300

    Добавила классы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,25 +7,23 @@ public class MyFirstProgram {
         hello("user");
         hello("Ulik");
 
-        double l = 180;
-        System.out.println("Площадь квадрата со стороной "+ l + " = " + area(l));
+        Square s = new Square(5);
 
-        double a = 4;
-        double b = 6;
-
-        System.out.println("Площадь пр-ка со сторонами "+ a + " и " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь квадрата со стороной "+ s.l + " = " + area(s));
+        System.out.println("Площадь пр-ка со сторонами "+ r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String smd) {
         System.out.println("Hello, " + smd +  "!");
     }
 
-    public static double area(double len) {
-        return len * len;
+    public static double area(Square s) {
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b){
-        return a * b;
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 
 }

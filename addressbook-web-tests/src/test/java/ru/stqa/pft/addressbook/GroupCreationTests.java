e2a951720d247a1904ba87b8c055c31ff16c3838
Uliana-Afonina <afonina6233@gmail.com>
2020-10-15 14:16:32 +0300

    Добавила небольшой автотест для браузер chrome

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class GroupCreationTests {
+
+    @Before
+    public void signUp() {
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("http://localhost/addressbook/group.php");
+    }
+
+    @Test
+
+
+    public void testGroupCreation() {
+        //input[@name='user']
+//        String text = selenium.getText( "xpath=(//input[@type='checkbox'])
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost/addressbook/group.php");
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+
+
+
+    }
+
+
+
+}
